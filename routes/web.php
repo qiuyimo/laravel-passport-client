a6b853bf9dbbbfea4741aa3dc8e0cd275dd490a4
qiuyu <530004000@qq.com>
2018-08-22 23:26:44 +0800

    laravel passport client

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,41 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * passport client test
+ */
+Route::get('/redirect', function () {
+    $query = http_build_query([
+        'client_id' => 3,
+        'redirect_uri' => 'http://laravel-passport-client.com/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+
+    return redirect('http://laravel-passport.com/oauth/authorize?'.$query);
+});
+
+/**
+ * passport callback
+ */
+Route::get('/callback', function (Request $request) {
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://laravel-passport.com/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => 3,
+            'client_secret' => 'KQ6XLIJFE73xXDuneqLZeCWEOTo1dyyX7jq5vmA7',
+            'redirect_uri' => 'http://laravel-passport-client.com/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
 });
